758f0a1d592333db48f52cbc9bff1b13306a99e0
animevn <haanhth21@gmail.com>
2020-03-25 15:27:10 +0700

    algorithm ok now

---
+++ src/Calculate.js
@@ -0,0 +1,27 @@
+export const Bmi = (weight, height)=>{
+  if (weight > 0 && height > 0){
+    return weight * 10000 / (height * height);
+  }else {
+    return "?"
+  }
+};
+
+export const BmiCategory = (bmi)=>{
+  if (bmi > 0){
+    if (bmi < 15){
+      return"too low";
+    }else if (bmi < 18){
+      return"low";
+    }else if (bmi < 25){
+      return"good";
+    }else if (bmi < 30){
+      return"overweight";
+    }else if (bmi < 35){
+      return"fat";
+    }else {
+      return"too fat";
+    }
+  }else {
+    return "Weight and height must larger than zero"
+  }
+};
